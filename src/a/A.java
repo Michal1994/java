1ac3f92ffe2f0fb9c5f328154a21885ac4624323
Michal1994 <pielkus@o2.pl>
2016-04-13 15:36:44 +0200

    Added files via upload

---
+++ src/a/A.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a;
+
+/**
+ *
+ * @author Student
+ */
+public class A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Tralala");
+    }
+}
